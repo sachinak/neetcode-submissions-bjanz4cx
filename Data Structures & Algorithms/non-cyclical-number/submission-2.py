057c6ba64f02978dc5e9ce03225dc8a96abdc086
sachinak <sachinkanth8@gmail.com>
2026-07-31 02:43:15 -0400

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        res = n
+        visit = set()
+        while True:
+            nums = [int(ch)*int(ch) for ch in str(res)]
+            res = sum(nums)
+            if res == 1:
+                return True
+            if res in visit:
+                return False
+            visit.add(res)
